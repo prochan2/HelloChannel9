2a9f0f6644721d1628548ec953288e7001b75f41
Antonín Procházka <antonin@postsharp.net>
2020-12-23 12:57:57 +0100

    Add project files.

---
+++ HelloChannel9/Program.cs
@@ -0,0 +1,6 @@
+﻿// Greeting for https://channel9.msdn.com/Blogs/3D-Printing/Where-to-get-the-Channel-9-Guy-3D-Model.
+// Sent via https://pohledniceonline.cz/.
+
+using System;
+
+Console.WriteLine("Hello Channel 9!");
